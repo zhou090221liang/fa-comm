04e27fd5920104bc9068ae51839a2871b6a2046e
zhouxiaoyue <zhouxiaoyue@zhouxiaoyuedeMacBook-Pro.local>
2020-02-14 16:32:52 +0800

    0.9.1

---
+++ test/req.js
@@ -0,0 +1,25 @@
+//http://xzqh.mca.gov.cn/map gb2312
+//
+const comm = require('../index');
+
+(async function test() {
+    // //获取民政局的省市区信息
+    // let url = 'http://xzqh.mca.gov.cn/map';
+    // console.log(url);
+    // let html = await comm.req.getHtml(url);
+    // console.log('请求html完成', url);
+    // let $ = await comm.req.getHtmlByJquery(url, 'gb2312');
+    // console.log('请求$完成', url);
+    // let arr = $('#pyArr').val();
+    // arr = JSON.parse(arr);
+    // console.log('获取民政局数据：', arr);
+
+    // //获取腾讯视频
+    // url = 'https://v.qq.com/x/search/?q=' + encodeURIComponent('将夜') + '&stag=0&smartbox_ab=';
+    // html = await comm.req.getHtml(url);
+    // console.log('请求html完成', url);
+    // $ = await comm.req.getHtmlByJquery(url);
+    // console.log('请求$完成', url);
+    // arr = $('.result_item_v');
+    // console.log('获取剧集完成：', arr);
+})();
