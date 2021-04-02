dbeacf085bff9f7685f190230cd2ebc10fc19482
zxy <zxy@zhouxiaoyue.cn>
2021-04-03 07:45:56 +0800

    1.1.1

--- test/m3u8.js
+++ test/m3u8.js
@@ -1,12 +1,25 @@
 (async function () {
     const path = require('path');
     const comm = require('../index');
+
     //将夜第一集（不完整）
     const url = 'https://video.buycar5.cn/20200905/mrrtZ4od/index.m3u8';
     //斗罗大陆第一集
     // const url = 'https://video.buycar5.cn/20200813/uNqvsBhl/index.m3u8';
-    // const m3u8Obj = await comm.m3u8.parse(url);
+
+    const m3u8Help = new comm.m3u8(url);
+
+    // //转换成对象
+    // const m3u8Obj = await m3u8Help.parse();
     // console.log(m3u8Obj);
 
-    await comm.m3u8.cache(url, path.join(__dirname, './m3u8chche/' + new Date().valueOf() + '/'), 16);
+    //缓存资源到本地1
+    m3u8Help.cache(path.join(__dirname, './m3u8chche/' + new Date().valueOf() + '/'), 16);
+    m3u8Help.cacheTaskProcess.on("process", function (data) {
+        console.log("process:", data);
+    });
+
+    // //缓存资源到本地2
+    // await m3u8Help.cache(path.join(__dirname, './m3u8chche/' + new Date().valueOf() + '/'), 16);
+    // console.log('完成');
 })();
